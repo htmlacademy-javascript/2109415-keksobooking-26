e458559c32fb192556b55a391f944a1d156a3692
ivmerk <ivan.e.merkulov@gmail.com>
2022-07-04 10:22:03 +0300

    fixing

--- js/mapmode.js
+++ js/mapmode.js
@@ -1,27 +1,35 @@
 const formElement = document.querySelector('.ad-form');
-formElement.classList.add('ad-form--disable');
-
-for (const child of formElement.children){
-  child.setAttribute('disabled', '');
-}
-
 const mapFilterElement = document.querySelector('.map__filters');
-mapFilterElement.classList.add('map__filters--disable');
 
-for (const child of mapFilterElement.children){
-  child.setAttribute('disabled', '');
+function turnOnDiableMode() {
+  formElement.classList.add('ad-form--disable');
+
+  for (const child of formElement.children) {
+    child.setAttribute('disabled', '');
+  }
+  mapFilterElement.classList.add('map__filters--disable');
+  for (const child of mapFilterElement.children) {
+    child.setAttribute('disabled', '');
+  }
 }
-// const mapElement = document.querySelector('.map__canvas');
-// mapElement.addEventListener('load', () => {
-formElement.classList.remove('ad-form--disable');
 
-for (const child of formElement.children){
-  child.removeAttribute('disabled');
+function tournOffDisableMode() {
+  // const mapElement = document.querySelector('.map__canvas');
+  // mapElement.addEventListener('load', () => {
+
+  formElement.classList.remove('ad-form--disable');
+
+  for (const child of formElement.children) {
+    child.removeAttribute('disabled');
+  }
+
+  mapFilterElement.classList.remove('map__filters--disable');
+
+  for (const child of mapFilterElement.children) {
+    child.removeAttribute('disabled');
+  }
+  // });
 }
 
-mapFilterElement.classList.remove('map__filters--disable');
-
-for (const child of mapFilterElement.children){
-  child.removeAttribute('disabled');
-}
-// });
+export{turnOnDiableMode};
+export{tournOffDisableMode};
